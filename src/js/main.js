7cf8402547f738db59ee3a3e163c48eddd95870f
sudoQ <vorga88@gmail.com>
2018-06-23 01:10:11 +0300

    fix picking range after next previous buttons clicked and on input

--- src/js/main.js
+++ src/js/main.js
@@ -5,22 +5,24 @@ window.addEventListener('load', function () {
 
         var wrapper;
 
-        var to;
+        var to; //current right calendar
 
         var yearTo;
         var monthTo;
-        var dayTo;
 
-        var from;
+        var dayTo; //object holds full day to
+
+        var from; //current left calendar
 
         var yearFrom;
         var monthFrom;
-        var dayFrom;
+
+        var dayFrom; //object holds full day from
 
         var inputFrom;
         var inputTo;
 
-        function init(obj,options) {
+        function init(obj, options) {
 
             wrapper = obj;
 
@@ -28,13 +30,15 @@ window.addEventListener('load', function () {
 
             yearTo = to.getFullYear();
             monthTo = to.getMonth();
-            dayTo = to.getDate();
+
+            dayTo = new Date(to.getFullYear(), to.getMonth(), to.getDate());
 
             from = new Date(yearTo, monthTo, 0);
 
             yearFrom = from.getFullYear();
             monthFrom = from.getMonth();
-            dayFrom = from.getDate();
+
+            dayFrom = new Date(yearFrom, monthFrom, dayTo.getDate());
 
             render();
         }
@@ -42,12 +46,22 @@ window.addEventListener('load', function () {
         function update(e) {
             var el = e.target;
             if (el.className.indexOf("from") != -1) {
-                dayFrom = parseInt(el.value);
+                var input = new Date(yearFrom, monthFrom, parseInt(el.value));
+
+                dayFrom = new Date(yearFrom, monthFrom, parseInt(el.value));
                 updateInputFrom(el);
+                wrapper.getElementsByClassName("calendar__both")[0].innerHTML = updateCalendar(yearFrom, monthFrom, 'from')
+                                        + updateCalendar(yearTo, monthTo, 'to');
+                addDayListener();
             }
             else if (el.className.indexOf("to") != -1) {
-                dayTo = parseInt(el.value);
+                var input = new Date(yearTo, monthTo, parseInt(el.value));
+
+                dayTo = new Date(yearTo, monthTo, parseInt(el.value));
                 updateInputTo(el);
+                wrapper.getElementsByClassName("calendar__both")[0].innerHTML = updateCalendar(yearFrom, monthFrom, 'from')
+                                        + updateCalendar(yearTo, monthTo, 'to');
+                addDayListener();
             }
         };
 
@@ -56,76 +70,130 @@ window.addEventListener('load', function () {
             wrapper.innerHTML = '';
             wrapper.innerHTML = `<div class='calendar__inputs'>From <input type='text' class='from'> To <input type='text' class='to'></div>`;
             wrapper.innerHTML += `<span class='calendar__prev'>Prev</span><span class='calendar__next'>Next</span>`;
-            wrapper.innerHTML += `<div class='calendar__both'>${updateCalendar(yearFrom, monthFrom, dayFrom, 'from')} ${updateCalendar(yearTo, monthTo, dayTo, 'to')}</div>`;
-            
+            wrapper.innerHTML += `<div class='calendar__both'>${updateCalendar(yearFrom, monthFrom, 'from')} ${updateCalendar(yearTo, monthTo, 'to')}</div>`;
+
             inputFrom = wrapper.getElementsByTagName('input')[0];
             inputTo = wrapper.getElementsByTagName('input')[1];
-            
-            addDayListener("calendar__from");
-            addDayListener("calendar__to");
+
+            inputFrom.focus();  //place cursor on load in from input
+
+            addDayListener();
             addNextPrevListener();
+            updateInputFrom();
+            updateInputTo();
+            addInputListener();
         }
 
-        function updateInputFrom(el) {
-            inputFrom.value = yearFrom + "-" + ("0" + (monthFrom + 1)).slice(-2)
-                + "-" + ("0" + dayFrom).slice(-2);
+        function updateInputFrom() {
+            inputFrom.value = dayFrom.getFullYear() + "-" + ("0" + (dayFrom.getMonth() + 1)).slice(-2)
+                + "-" + ("0" + dayFrom.getDate()).slice(-2);
         }
 
-        function updateInputTo(el) {
-            inputTo.value = yearFrom + "-" + ("0" + (monthTo + 1)).slice(-2)
-                + "-" + ("0" + dayTo).slice(-2);
+        function updateInputTo() {
+            inputTo.value = dayTo.getFullYear() + "-" + ("0" + (dayTo.getMonth() + 1)).slice(-2)
+                + "-" + ("0" + dayTo.getDate()).slice(-2);
         }
 
-        function addDayListener(calendar1, calendar2) {
+        function addInputListener() {
+            inputFrom.addEventListener('input', handleInput);
+            inputTo.addEventListener('input', handleInput);
 
-            var calDays = document.getElementsByClassName(calendar1)[0].getElementsByClassName("day");
+            function handleInput(e) {
+                if (e.target.value.length == 10) {
+                    var arr = e.target.value.split("-");
+                    var year = parseInt(arr[0]);
+                    var month = parseInt(arr[1]);
+                    var day = parseInt(arr[2]);
 
-            for (var n = 0; n < calDays.length; n++) {
-                calDays[n].addEventListener('click', update);
+                    if (year > 2000 && year < new Date().getFullYear() + 1) {
+                        if (month >= 0 && month < 12) {
+                            if (day > 0 && day < 32) {
+
+                                if (e.target == inputFrom) {
+                                    from = new Date(year, month - 1, day);
+                                    yearFrom = from.getFullYear();
+                                    monthFrom = from.getMonth();
+
+                                    dayFrom = new Date(yearFrom, monthFrom, from.getDate());
+
+                                    to = new Date(yearFrom, monthFrom + 1, 1);
+                                    yearTo = to.getFullYear();
+                                    monthTo = to.getMonth();
+
+                                    wrapper.getElementsByClassName("calendar__both")[0].innerHTML = updateCalendar(yearFrom, monthFrom, 'from')
+                                        + updateCalendar(yearTo, monthTo, 'to');
+                                        addDayListener();
+                                }
+
+                                else if (e.target == inputTo) {
+                                    to = new Date(year, month - 1, day);
+                                    yearTo = to.getFullYear();
+                                    monthTo = to.getMonth();
+                                    dayTo = new Date(yearTo, monthTo, to.getDate());
+
+                                    from = new Date(yearTo, monthTo - 1, 1);
+                                    yearFrom = from.getFullYear();
+                                    monthFrom = from.getMonth();
+
+                                    wrapper.getElementsByClassName("calendar__both")[0].innerHTML = updateCalendar(yearFrom, monthFrom, 'from')
+                                        + updateCalendar(yearTo, monthTo, 'to');
+                                    addDayListener();
+                                }
+
+                            }
+                        }
+                    }
+
+                }
             }
+        }
 
-            if (calendar2) {
-                var calDays = document.getElementsByClassName(calendar2)[0].getElementsByClassName("day");
+        function addDayListener() {
+
+                var calDays = document.getElementsByClassName("calendar__from")[0].getElementsByClassName("day");
 
                 for (var n = 0; n < calDays.length; n++) {
                     calDays[n].addEventListener('click', update);
                 }
-            }
+
+                var calDays = document.getElementsByClassName("calendar__to")[0].getElementsByClassName("day");
+
+                for (var n = 0; n < calDays.length; n++) {
+                    calDays[n].addEventListener('click', update);
+                }
 
         }
 
         function addNextPrevListener() {
             wrapper.getElementsByClassName("calendar__prev")[0].addEventListener('click', function () {
-                to = from;
-                yearTo = yearFrom;
-                monthTo = monthFrom;
-
-                from = new Date(yearFrom, monthFrom, 0);
-                yearFrom = from.getFullYear();
-                monthFrom = from.getMonth();
-                wrapper.getElementsByClassName("calendar__both")[0].innerHTML = updateCalendar(yearFrom, monthFrom, dayFrom, 'from') 
-                                                                                + updateCalendar(yearTo, monthTo, dayTo, 'to'); 
-                addDayListener("calendar__from");
-                addDayListener("calendar__to");
-            });
-            wrapper.getElementsByClassName("calendar__next")[0].addEventListener('click', function () {
-                from = to;
-                yearFrom = yearTo;
-                monthFrom = monthTo;
-
-                to = new Date(yearTo, monthFrom + 1, 1);
+                to = new Date(yearTo, monthTo-1, 0);
                 yearTo = to.getFullYear();
                 monthTo = to.getMonth();
 
-                wrapper.getElementsByClassName("calendar__both")[0].innerHTML = updateCalendar(yearFrom, monthFrom, dayFrom, 'from') 
-                                                                                + updateCalendar(yearTo, monthTo, dayTo, 'to');
-                addDayListener("calendar__from");
-                addDayListener("calendar__to");
+                from = new Date(yearFrom, monthFrom-1, 0);
+                yearFrom = from.getFullYear();
+                monthFrom = from.getMonth();
+                wrapper.getElementsByClassName("calendar__both")[0].innerHTML = updateCalendar(yearFrom, monthFrom, 'from')
+                    + updateCalendar(yearTo, monthTo, 'to');
+                addDayListener();
+            });
+            wrapper.getElementsByClassName("calendar__next")[0].addEventListener('click', function () {
+                from = new Date(yearFrom, monthFrom+ 1);
+                yearFrom = from.getFullYear();;
+                monthFrom = from.getMonth();
+
+                to = new Date(yearTo, monthTo + 1);
+                yearTo = to.getFullYear();
+                monthTo = to.getMonth();
+
+                wrapper.getElementsByClassName("calendar__both")[0].innerHTML = updateCalendar(yearFrom, monthFrom, 'from')
+                    + updateCalendar(yearTo, monthTo, 'to');
+                addDayListener();
             });
         }
 
 
-        function updateCalendar(year, month, day, toOrFrom) {
+        function updateCalendar(year, month, toOrFrom) {
 
             var calendar = `<div class='calendar__${toOrFrom}'> 
                                     <div class='calendar__header'>
@@ -143,25 +211,29 @@ window.addEventListener('load', function () {
             offset = offset == 0 ? 7 : offset; // 0 is Sunday
 
             for (var i = 1; i <= days; i++) {
-                var selection;
+                var selection = '';
 
-                if (i == day) {
-                    selection = 'selected';
+                var currentDay = new Date(year, month, i);
+
+                if (currentDay - dayFrom == 0) {
+                    selection = 'selected dayfrom';
                 }
 
-                else if (i < day) {
-                    selection = 'less';
+                else if (currentDay - dayTo == 0) {
+                    selection = 'selected dayto';
+                    console.log(+dayTo);
+                    console.log(+currentDay);
                 }
 
-                else {
-                    selection = 'more';
+                else if (currentDay - dayFrom > 0 && dayTo - currentDay > 0) {
+                    selection = 'inrange';
                 }
 
                 if (i == 1) {
-                    calendar += `<input type="button" class='day ${toOrFrom} ${selection}' style='grid-col-start:${offset};' value='${i}'>`;
+                    calendar += `<button class='day ${toOrFrom} ${selection}' style='grid-col-start:${offset};' value='${i}'>${i}</button>`;
                 }
                 else {
-                    calendar += `<input type="button" class='day ${toOrFrom} ${selection}' value='${i}'>`;
+                    calendar += `<button class='day ${toOrFrom} ${selection}' value='${i}'>${i}</button>`;
                 }
 
             }
